143d1e6d511372f670f7514de32dc3118b339669
Dmytro Savchuk <dmytrosavchuk@MacBook-Air-Dmytro.local>
2019-01-23 10:37:22 +0200

    eb try to follow commits to automatically deployment

---
+++ server.js
@@ -0,0 +1,15 @@
+// server.js
+const express = require('express');
+const app = express();
+const db = "mongodb://admin:admin@127.0.0.1:27017/sample-test";
+const mongoose = require("mongoose");
+mongoose.connect(db).then(() => console.log(`Connected to ${db}...`));
+
+app.get('/', (req, res) => {
+    res.send('Hesoyan!!!');
+});
+
+
+app.listen(3000, () => {
+    console.log('Server is up on 3000')
+});
